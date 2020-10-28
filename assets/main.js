71f01c93b869b84771f403b6f76d7cb10301741b
Marco Guidi <mark.88g@gmail.com>
2020-10-28 18:48:17 +0100

    HEADER SISTEMAZIONE STILE

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,21 @@
 $("document").ready(function() {
 
-    // dropdown-function: on click
+    // DROPDOWN-FUNCTION
+    // mouse enter --> dropdown visible
     $(".dropdown-item").mouseenter(function() {
         $(this).children(".dropdown-menu").addClass("visible");
     });
+    // mouse leave --> dropdown not-visible
     $(".dropdown-item").mouseleave(function() {
         $(this).children(".dropdown-menu").removeClass("visible");
+    });
+
+    // HAMBURGER-FUNCTION
+    // on click --> on/off
+    $(".hamburger-menu i").click(function() {
+        // icon hamburger-cross: on-off
+        $(".hamburger-menu i").toggleClass("visible");
+        // // header-menu --> visible
+        // $(".header-menu").toggleClass("visible");
     });
 });
