399c4d6602978721136375b290084727fec3f33b
Marco Guidi <mark.88g@gmail.com>
2020-10-28 17:10:40 +0100

    HEADER RESPONSIVE

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,5 @@
 $("document").ready(function() {
 
-    alert("prova");
     // dropdown-function: on click
     $(".dropdown-item").mouseenter(function() {
         $(this).children(".dropdown-menu").addClass("visible");
