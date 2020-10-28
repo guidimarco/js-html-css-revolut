9de0c3e8d11faedb4fc909a3b9d773f102739ec7
Marco Guidi <mark.88g@gmail.com>
2020-10-28 15:48:15 +0100

    REALIZZATA FUNZIONE VISUALIZZAZIONE DROPDOWN MENU

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,11 @@
 $("document").ready(function() {
 
-
-};
+    alert("prova");
+    // dropdown-function: on click
+    $(".dropdown-item").mouseenter(function() {
+        $(this).children(".dropdown-menu").addClass("visible");
+    });
+    $(".dropdown-item").mouseleave(function() {
+        $(this).children(".dropdown-menu").removeClass("visible");
+    });
+});
